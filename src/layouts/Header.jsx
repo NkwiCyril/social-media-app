18a8f4f29c394213bdba59c42eeb3943fb2a050b
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-01 09:02:25 +0100

    Included navbar to header component

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Header/Navbar";
+
+function Header() {
+  return (
+    <>
+      <header className="w-full bg-primary px-1">
+        <Navbar />
+      </header>
+    </>
+  );
+}
+
+export default Header;
