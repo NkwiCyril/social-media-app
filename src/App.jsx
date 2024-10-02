2f3d86fdb5d66093808d5aca588d39eba0f55ac1
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-02 17:42:12 +0100

    Modified css design and app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './styles/index.css'
-
+import Header from "./layouts/Header";
+import LeftSection from "./layouts/LeftSection";
+import RightSection from "./layouts/RightSection";
+import CenterSection from "./layouts/CenterSection";
+import "./styles/index.css";
 
 function App() {
-
   return (
-   <p className="">
-    Here we go again
-   </p> 
-  )
+    <>
+      <Header />
+      <div className="flex space-x-6 py-5 px-5">
+        <div className="w-1/4 h-fit">
+          <LeftSection />
+        </div>
+        <div className="w-1/2">
+          <CenterSection />
+        </div>
+        <div className="w-1/4 h-fit">
+          <RightSection />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
