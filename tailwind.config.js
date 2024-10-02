0d8090d31b65915455e010e3ae5d94e6c63ed24d
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-02 17:46:39 +0100

    tailwindcss cnfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundColor: "#131415",
       fontFamily: {
-        sans: ['Hanken Grotesk', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'], 
+        sans: [
+          "Hanken Grotesk",
+          "system-ui",
+          "Avenir",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
       },
       colors: {
-        primary: '#323539',
-        accent: '#ffd37d',
-        background: '#131415',
-        textColor: '#d9e4ed',
+        primary: "#1e2125",
+        secondary: "#818795",
+        darkSecondary: "#323539",
+        accent: "#ffd37d",
+        background: "#131415",
+        textColor: "#fff",
+        btnBg: "#564c38"
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("tailwindcss-glassmorphism")
+  ],
+};
