e58d14bcc4e4a64e63d48c8294bed483b4d21dc9
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-02 11:22:56 +0100

    Modified height and width of image

--- src/components/LeftSection/Profile.jsx
+++ src/components/LeftSection/Profile.jsx
@@ -7,7 +7,7 @@ function Profile({image, fullName, userName}) {
         <img
           src={image}
           alt="Profile"
-          className="rounded-full border-4 border-accent object-cover w-24 h-24"
+          className="rounded-full border-4 p-0.5 border-accent object-cover w-24 h-24"
         />
         <h2 className="text-lg mt-1">{fullName}</h2>
         <p className="text-gray-400">@{userName}</p>
