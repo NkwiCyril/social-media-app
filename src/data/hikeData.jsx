2e043b51d4f4f2b636111c12db1c7dc78b7f29ba
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-03 13:17:31 +0100

    Included functions in utils file

--- src/data/hikeData.jsx
+++ src/data/hikeData.jsx
@@ -1,13 +1,4 @@
-function getRandomDate() {
-  const start = new Date(2024, 0, 1);
-  const end = new Date(2024, 11, 31);
-  const randomDate = new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-
-  const options = { month: "short", day: "numeric", weekday: "short" };
-  return randomDate.toLocaleDateString("en-US", options);
-}
+import { getRandomDate } from "../lib/utils";
 
 const hikeData = [
   {
